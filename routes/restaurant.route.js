1ead4f7ad53d76a3ad214fbb39217feb6646f6fe
Vinay h c <149578035+Vinay-webDev@users.noreply.github.com>
2025-02-16 12:20:14 +0530

    Update restaurant.route.js

--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { createRestaurant, deleteRestaurant } from "../controllers/restaurant.controller.js";
+import { createRestaurant, deleteRestaurant, getAllRestaurant, updateRestaurant } from "../controllers/restaurant.controller.js";
 const router = express.Router();
 
 router.post('/create', createRestaurant);
 router.delete('/delete', deleteRestaurant);
+router.get('/get', getAllRestaurant);
+router.put('/update', updateRestaurant);
 
 export default router;
